410f690b2106768f02f4fe436fe3c7692f1822e6
Justin Searls <searls@gmail.com>
2016-05-21 14:12:30 -0400

    Write an indecisive test

---
+++ smells/unclear/indecisive.js
@@ -0,0 +1,47 @@
+/* Smell: Indecisive Tests
+ *
+ * primitive obsession / lack of good test controls
+ */
+
+// Subject under test
+function joinPath(fragments) {
+  var separator, pattern
+  if (process.platform == 'win32') {
+    separator = '\\'
+    pattern = /\\+/g
+  } else {
+    separator = '/'
+    pattern = /\/+/g
+  }
+  return fragments.join(separator).replace(pattern, separator)
+}
+
+// Test
+module.exports = {
+  simpleCase: function () {
+    var fragments = ['foo', 'bar', 'baz']
+
+    var result = joinPath(fragments)
+
+    if (process.platform == 'win32') {
+      assert.equal(result, 'foo\\bar\\baz')
+    } else {
+      assert.equal(result, 'foo/bar/baz')
+    }
+  },
+  containsSeparators: function () {
+    if (process.platform == 'win32') {
+      var fragments = ['\\foo\\', 'bar\\biz', 'baz\\']
+    } else {
+      var fragments = ['/foo/', 'bar/biz', 'baz/']
+    }
+
+    var result = joinPath(fragments)
+
+    if (process.platform == 'win32') {
+      assert.equal(result, '\\foo\\bar\\biz\\baz\\')
+    } else {
+      assert.equal(result, '/foo/bar/biz/baz/')
+    }
+  }
+}
