21498b0af8584c512e60efc03327d1c06d43efd4
Justin Searls <searls@gmail.com>
2016-05-21 12:21:33 -0400

    Add a complex assertion.
    I don't really love this example tbh.

---
+++ smells/unclear/complex-assertions.js
@@ -0,0 +1,61 @@
+/* Smell:
+ */
+
+// Subject under test
+var _ = require('lodash')
+function incrementAge (people) {
+  return _(_.cloneDeep(people)).map(function (person) {
+    if(_.isNumber(person.age)) {
+      person.age += 1
+    }
+    if(_.isArray(person.kids)) {
+      person.kids = incrementAge(person.kids)
+    }
+    return person
+  }).shuffle().value()
+}
+
+// Test
+module.exports = {
+  incrementsSinglePersonAge: function () {
+    var people = [
+      {name: 'Jane', age: 39},
+      {name: 'John', age: 99}
+    ]
+    var results = incrementAge(people)
+
+    var jane = results.find(function (person) { return person.name == 'Jane' })
+    assert.equal(jane.age, 40)
+    var john = results.find(function (person) { return person.name == 'John' })
+    assert.equal(john.age, 100)
+
+    assert.includes(results, {name: 'Jane', age: 40})
+    assert.includes(results, {name: 'John', age: 100})
+  },
+  incrementsKidsAgeToo: function () {
+    var people = [
+      {name: 'Jane', age: 39},
+      {name: 'Joe', age: 42, kids: [
+        {name: 'Jack', age: 8},
+        {name: 'Jill', age: 7}
+      ]}
+    ]
+
+    var results = incrementAge(people)
+
+    var jane = results.find(function (person) { return person.name == 'Jane' })
+    assert.equal(jane.age, 40)
+    var joe = results.find(function (person) { return person.name == 'Joe' })
+    assert.equal(joe.age, 43)
+    var jack = joe.kids.find(function (person) { return person.name == 'Jack' })
+    assert.equal(jack.age, 9)
+    var jill = joe.kids.find(function (person) { return person.name == 'Jill' })
+    assert.equal(jill.age, 8)
+
+    assert.includes(results, {name: 'Jane', age: 40})
+    assert.includes(results, {name: 'Joe', age: 43, kids: [
+      {name: 'Jack', age: 9},
+      {name: 'Jill', age: 8}
+    ]})
+  }
+}
--- support/decorate-assertions.js
+++ support/decorate-assertions.js
@@ -1,19 +1,19 @@
 var _ = require('lodash')
+var stringify = require('stringify-object-with-one-liners')
 
 module.exports = function (assert) {
   return _.extend({}, assert, {
     includes: function (actual, expected) {
-      if (!_.includes(actual, expected)) {
+      if (!_.includes(actual, expected) && !_.some(actual, expected)) {
         throw new Error(
-          'AssertionError: expected:\n\n"' + actual +
-          '"\n\n to contain:\n\n"' + expected + '"\n\n'
+          'AssertionError: expected: ' + stringify(actual, {indent: '  '}) +
+          '\n\nto contain: ' + stringify(expected, {indent: '  '}) + '\n\n'
         )
       }
     },
     codePricing: function (actual, code) {
       // See: smells/unclear/chafing.js
       var expected = code.charCodeAt(0) * Math.round((1000 - (code.length * 39)) / 42)
-      console.log(expected)
       assert.equal(actual, expected)
     }
 
