f05826fbc09684f0717cc7b109382db4e18c4ec7
Justin Searls <searls@gmail.com>
2016-05-21 08:46:59 -0400

    Add a quixotic test

--- smells/insufficient/quixotic.js
+++ smells/insufficient/quixotic.js
@@ -2,6 +2,28 @@
  */
 
 // Subject under test
+function curl (url, cb) {
+  get(url, function (er, data) {
+    if (er) return cb(er)
+    cb(null, 'URL: ' + url + ' returned: "' + data + '"')
+  })
+}
 
 // Test
-module.exports = {}
+module.exports = {
+  happyPath: function (done) {
+    curl('https://google.com', function (er, data) {
+      assert.equal(data, 'URL: https://google.com returned: "some data!"')
+      done(er)
+    })
+  }
+}
+
+// Fake production implementations to simplify example test of subject
+function get (url, cb) {
+  if (url.indexOf('https') !== 0) {
+    cb(new Error('SSL only!'))
+  } else {
+    cb(null, 'some data!')
+  }
+}
