ce48cd3dfc746aa8ebbc9c26cfacfaac6ce4564f
Justin Searls <searls@gmail.com>
2016-05-22 09:39:35 -0400

    Add a test with premature assertions

---
+++ smells/unnecessary/premature-assertions.js
@@ -0,0 +1,51 @@
+/* Smell: Premature Assertions
+ */
+
+// Subject under test
+function Plane (airTrafficControl) {
+  this.airTrafficControl = airTrafficControl
+}
+Plane.prototype.takeOff = function () {
+  if (this.isStarted() &&
+      this.onAirstrip() &&
+      this.airTrafficControl.readyForTakeoff()) {
+    return Math.round(Math.random() * 1000)
+  } else {
+    return 0
+  }
+}
+
+// Test
+module.exports = {
+  beforeEach: function () {
+    this.airTrafficControl = new AirTrafficControl()
+    this.plane = new Plane(this.airTrafficControl)
+  },
+  takeOffWhenReady: function () {
+    this.plane.start()
+    assert.equal(this.plane.isStarted(), true)
+    this.plane.taxi()
+    assert.equal(this.plane.onAirstrip(), true)
+    this.airTrafficControl.approve(this.plane)
+    assert.equal(this.airTrafficControl.readyForTakeoff(this.plane), true)
+
+    var altitude = this.plane.takeOff()
+
+    assert.ok(altitude > 0, 'altitude should be greater than 0')
+  },
+  doesNotTakeOffWhenNotReady: function () {
+    var altitude = this.plane.takeOff()
+
+    assert.equal(altitude, 0)
+  }
+}
+
+// Fake production implementations to simplify example test of subject
+Plane.prototype.start = function () { this.started = true }
+Plane.prototype.isStarted = function () { return this.started }
+Plane.prototype.taxi = function () { this.taxied = true }
+Plane.prototype.onAirstrip = function () { return this.taxied }
+
+function AirTrafficControl () {}
+AirTrafficControl.prototype.approve = function (plane) { this.approved = true }
+AirTrafficControl.prototype.readyForTakeoff = function (plane) { return this.approved }
