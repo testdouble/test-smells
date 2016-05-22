9b77d21c8fd77c0b6e47f035afe4abd45c0f92a3
Justin Searls <searls@gmail.com>
2016-05-22 10:18:08 -0400

    wow _.some really doesn't work like i'd expect
    wish i just had a lodash method for object contains other object. did
    they just straight-up remove this?
    
    cc/ @jasonkarns :trollface:

--- support/decorate-assertions.js
+++ support/decorate-assertions.js
@@ -4,7 +4,9 @@ var stringify = require('stringify-object-with-one-liners')
 module.exports = function (assert) {
   return _.extend({}, assert, {
     includes: function (actual, expected) {
-      if (!_.includes(actual, expected) && !_.some(actual, expected)) {
+      if (!_.includes(actual, expected) &&
+          !_.some(actual, expected) &&
+          !_.some([actual], expected)) {
         throw new Error(
           'AssertionError: expected: ' + stringify(actual, {indent: '  '}) +
           '\n\nto contain: ' + stringify(expected, {indent: '  '}) + '\n\n'
