a4604a96667edb52ed6a19a6929d345f97f5bc03
Justin Searls <searls@gmail.com>
2016-05-22 12:31:32 -0400

    Add a test for contaminated test subject

---
+++ smells/unrealistic/contaminated-test-subject.js
@@ -0,0 +1,65 @@
+/* Smell: Contaminated Test Subject
+ */
+
+// Subject under test
+function SavingsBond (startDate, matureValue, termInYears) {
+  this.startDate = startDate
+  this.matureValue = matureValue
+  this.termInYears = termInYears
+  this.today = new Date(Date.now())
+}
+
+SavingsBond.prototype.currentValue = function () {
+  if (this.isMature()) {
+    return this.matureValue
+  } else {
+    var elapsed = this.today.getTime() - this.startDate.getTime()
+    var termDuration = this.matureDate().getTime() - this.startDate.getTime()
+
+    return Math.round(this.matureValue * (elapsed / termDuration))
+  }
+}
+
+// Test
+var td = require('testdouble')
+module.exports = {
+  beforeEach: function () {
+    this.subject = new SavingsBond(
+      new Date(2000, 5, 15),
+      1000,
+      10
+    )
+  },
+  whenMatureCurrentValueIsMatureValue: function () {
+    td.when(td.replace(this.subject, 'isMature')()).thenReturn(true)
+
+    var result = this.subject.currentValue()
+
+    assert.equal(result, 1000)
+  },
+  whenImmatureCurrentValueIsProrated: function () {
+    td.when(td.replace(this.subject, 'isMature')()).thenReturn(false)
+    td.replace(this.subject, 'today', new Date(2005, 5, 15))
+
+    var result = this.subject.currentValue()
+
+    assert.equal(result, 500)
+  },
+  afterEach: function () {
+    td.reset()
+  }
+}
+
+// Fake production implementations to simplify example test of subject
+SavingsBond.prototype.isMature = function () {
+  return this.matureDate.getTime() - Date.now() > 0
+}
+
+SavingsBond.prototype.matureDate = function () {
+  return new Date(this.startDate.getTime() + this.termInMs())
+}
+
+SavingsBond.prototype.termInMs = function () {
+  return this.termInYears * 365.25 * 24 * 60 * 60 * 1000
+}
+
