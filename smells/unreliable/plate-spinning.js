989f17b875a87a398b9ae1f4f8b4d2d90879faad
Justin Searls <searls@gmail.com>
2016-05-23 16:25:42 -0400

    Created an unreliable async test
    This thing should fail roughly 20% of the time. Yay.

---
+++ smells/unreliable/plate-spinning.js
@@ -0,0 +1,61 @@
+/* Smell: Plate-spinning
+ */
+
+// Subject under test
+function download (path, cb) {
+  get(path, function (er, value) {
+    if (er) return cb(er)
+    insert(value.id, value, function (er) {
+      cb(er)
+    })
+  })
+}
+
+// Test
+module.exports = {
+  beforeEach: function () {
+    post('/interests', {id: 42, interests: ['Ruby on Rails']}, function(){})
+  },
+  getsAndThenInserts: function (done) {
+    download('/interests', function (er) {
+      select(42, function (er, value) {
+        assert.deepEqual(value, {id: 42, interests: ['Ruby on Rails']})
+        done(er)
+      })
+    })
+  }
+}
+
+// Fake production implementations to simplify example test of subject
+var _ = require('lodash')
+var resources = {}
+function post (path, value, cb) {
+  randomTimeout(function () {
+    resources[path] = value
+    cb(null)
+  }, 10, 30)
+}
+
+function get (path, cb) {
+  randomTimeout(function () {
+    cb(null, resources[path])
+  }, 15, 50)
+}
+
+var db = {}
+function insert (id, value, cb) {
+  randomTimeout(function () {
+    db[id] = value
+    cb(null)
+  })
+}
+
+function select (id, cb) {
+  randomTimeout(function () {
+    cb(null, db[id])
+  })
+}
+
+function randomTimeout(func, low, high) {
+  setTimeout(func, _(low || 10).range(high || 100).sample())
+}
