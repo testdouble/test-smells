eaf306f5fe3dcbe22ef53cc83ae73c696c549833
Justin Searls <searls@gmail.com>
2016-05-23 15:06:03 -0400

    Add litter bug test with 2 kinds of pollution!

---
+++ smells/unreliable/litter-bugs.js
@@ -0,0 +1,36 @@
+/* Smell: Litter Bugs
+ */
+
+// Subject under test
+var allTimeLogins = 0
+
+function Game () {
+  this.players = []
+}
+
+Game.prototype.addPlayer = function (name) {
+  this.players.push(name)
+  allTimeLogins++
+}
+
+Game.prototype.playerCount = function () {
+  return this.players.length
+}
+
+// Test
+var game = new Game()
+module.exports = {
+  loginOnePlayer: function () {
+    game.addPlayer('Joe')
+
+    assert.equal(game.playerCount(), 1)
+    assert.equal(allTimeLogins, 1)
+  },
+  loginTwoPlayers: function () {
+    game.addPlayer('Jane')
+    game.addPlayer('Stef')
+
+    assert.equal(game.playerCount(), 3)
+    assert.equal(allTimeLogins, 3)
+  }
+}
