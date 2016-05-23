cf0d263980ad274e7e047edb354b7eeff901a159
Justin Searls <searls@gmail.com>
2016-05-23 14:56:56 -0400

    add a fire-and-forget example

---
+++ smells/unreliable/fire-and-forget.js
@@ -0,0 +1,29 @@
+/* Smell: Fire and Forget
+ */
+
+// Subject under test
+function loadUser (id, cb) {
+  var path = '/users/' + id
+
+  get(path, function (er, user) {
+    user.resolvedVia = path
+    cb(er, user)
+  })
+}
+
+// Test
+module.exports = {
+  getsUserAndDecoratesPath: function () {
+    loadUser(42, function (er, user) {
+      assert.equal(user.resolvedVia, '/users/42')
+      assert.equal(user.name, 'Jo')
+    })
+  }
+}
+
+// Fake production implementations to simplify example test of subject
+function get (path, cb) {
+  setTimeout(function () {
+    cb(null, {name: 'Jo'})
+  }, 10)
+}
