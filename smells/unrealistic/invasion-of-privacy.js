156c71eafa111fa6bf1e72204235cb986173674e
Justin Searls <searls@gmail.com>
2016-05-23 09:47:58 -0400

    update note

--- smells/unrealistic/invasion-of-privacy.js
+++ smells/unrealistic/invasion-of-privacy.js
@@ -1,8 +1,8 @@
 /* Smell: Invasion of Privacy
  *
- * In the example below, the test of the private function drove out two if
- * statements that are simultaneously unreachable in production (e.g. dead code)
- * that nevertheless have 100% code coverage (e.g. no one will ever feel safe
+ * In the example below, the test of the private function drove out two
+ * if-statements that are simultaneously unreachable in production (e.g. dead
+ * code) that nevertheless have 100% code coverage (e.g. no one will ever feel safe
  * deleting them).
  *
  * See: unnecessary/paranoid
@@ -13,7 +13,7 @@
  * private from users of the object.]
  *
  * [Note 2: Remember, privacy is not about what's technically reachable, it's
- * what the author intends to prevent others coming to depend on so that the
+ * what the author intends to prevent others from depending on so that the
  * implementation can be freely changed in the future!]
  */
 
