428cdfdcaece102e376fe7d337b30ee2101670c7
Todd Kaufman <toddkaufman@gmail.com>
2016-05-24 14:05:44 -0400

    Solution for invisible assertions

--- smells/insufficient/invisible-assertions.js
+++ smells/insufficient/invisible-assertions.js
@@ -29,7 +29,9 @@ module.exports = {
   is21: function () {
     var person = { age: 21 }
 
-    is21(person)
+    assert.doesNotThrow(function() {
+      is21(person)
+    })
   },
   isUnderAge: function () {
     var person = { age: 20 }
