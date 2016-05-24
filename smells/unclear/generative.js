8aede8a0583b4d01fd988efe02cb182934ccb9ba
Todd Kaufman <toddkaufman@gmail.com>
2016-05-24 17:05:59 -0400

    Solution for generative tests

--- smells/unclear/generative.js
+++ smells/unclear/generative.js
@@ -3,36 +3,41 @@
 
 // Subject under test
 var _ = require('lodash')
+var ROMAN_VALUES = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000
+}
+
 function toArabic (roman) {
   return _(roman).map(function (x, i) {
-    var nextX = roman[i + 1]
-    if (x === 'I') {
-      return _.includes(['V', 'X'], nextX) ? -1 : 1
-    } else if (x === 'V') {
-      return nextX === 'X' ? -5 : 5
-    } else if (x === 'X') {
-      return nextX === 'C' ? -10 : 10
-    }
+    var val = ROMAN_VALUES[x]
+    var nextChar = roman[i + 1]
+    var nextVal = ROMAN_VALUES[nextChar]
+    return nextVal > val ? -val : val
   }).reduce(function (memo, x) { return memo + x })
 }
 
 // Test
-module.exports = _.transform({
-  'I': 1,
-  'II': 2,
-  'III': 3,
-  'IV': 4,
-  'V': 5,
-  'VI': 6,
-  'VII': 7,
-  'VIII': 8,
-  'IX': 9,
-  'X': 10
-}, function (test, arabic, roman) {
-  test['Roman numeral: ' + roman + ' => ' + arabic] = function () {
-    assert.equal(arabic, toArabic(roman))
+module.exports = {
+
+  IShouldReturn1: function() {
+    assert.equal(1, toArabic('I'))
+  },
+
+  IVShouldReturn4: function() {
+    assert.equal(4, toArabic('IV'))
+  },
+
+  MCDXLVIIIShouldReturn1448: function() {
+    assert.equal(1448, toArabic('MCDXLVIII'))
   }
-})
+
+}
 
 // Fake production implementations to simplify example test of subject
 
