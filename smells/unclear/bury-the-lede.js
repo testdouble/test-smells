4f39cdf1088eb97bbf874485d4d0491cd1dd969d
Todd Kaufman <toddkaufman@gmail.com>
2016-05-24 14:43:13 -0400

    First pass at solution for bury the lede

--- smells/unclear/bury-the-lede.js
+++ smells/unclear/bury-the-lede.js
@@ -6,37 +6,43 @@ function verifyCardholderZip (cardId, zip) {
   var card = repo.find(cardId)
   var user = repo.find(card.userId)
   var address = repo.find(user.addressId)
-
+  //TODO: Match on full or first five, goal to extract method
   return address.zip === zip
 }
 
 // Test
 module.exports = {
   trueIfZipMatches: function () {
-    var address = {street: '123 Sesame', city: 'Cbus', state: 'OH', zip: '42'}
-    repo.save(address)
-    var user = {
-      addressId: address.id,
-      name: 'Jane',
-      age: 12,
-      income: '$12.48'
-    }
-    repo.save(user)
-    var issuer = {bankName: 'Bank Co'}
-    repo.save(issuer)
-    var card = {
-      userId: user.id,
-      apr: 17.8,
-      number: '1234 0000 2828 4494',
-      ccv: 364,
-      issuerId: issuer.id
-    }
-    repo.save(card)
+    var card = createFakeCardWithZip('32141');
 
-    var result = verifyCardholderZip(card.id, '42')
+    var result = verifyCardholderZip(card.id, '32141')
 
     assert.equal(result, true)
   }
+}
+
+// Test Helper
+function createFakeCardWithZip (zip) {
+  var address = {street: '123 Sesame', city: 'Cbus', state: 'OH', zip: zip}
+  repo.save(address)
+  var user = {
+    addressId: address.id,
+    name: 'Jane',
+    age: 12,
+    income: '$12.48'
+  }
+  repo.save(user)
+  var issuer = {bankName: 'Bank Co'}
+  repo.save(issuer)
+  var card = {
+    userId: user.id,
+    apr: 17.8,
+    number: '1234 0000 2828 4494',
+    ccv: 364,
+    issuerId: issuer.id
+  }
+  repo.save(card)
+  return card
 }
 
 // Fake production implementations to simplify example test of subject
