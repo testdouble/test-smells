aaed082ab6ac5d5759844dabb71d51a78dcc3aa8
Justin Searls <searls@gmail.com>
2016-05-25 12:03:18 -0400

    Start docs on this

--- smells/unclear/chafing.js
+++ smells/unclear/chafing.js
@@ -1,6 +1,12 @@
 /* Smell: Chafing Tests
  *
- * Tests so DRY they chafe
+ * Odor: A test in which the author attempts to eliminate as much textual
+ *       duplication as possible, even if the indirection it introduces confuses
+ *       future readers of the intention and behavior of the test
+ *
+ * Known causes:
+ *
+ * Example notes:
  */
 
 // Subject under test
