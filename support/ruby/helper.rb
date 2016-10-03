840bca3a89705e1839e8681643a3461ea53bb66b
Justin Searls <searls@gmail.com>
2016-10-03 11:29:59 +0900

    Fix 1.9.3

--- support/ruby/helper.rb
+++ support/ruby/helper.rb
@@ -3,7 +3,7 @@ require "pry"
 
 class SmellTest < Minitest::Test
   def assert_code_pricing(code, actual)
-    expected = code.codepoints[0] * ((1000 - (code.length * 39.0)) / 42).round
+    expected = code.codepoints.first * ((1000 - (code.length * 39.0)) / 42).round
 
     assert_equal expected, actual
   end
