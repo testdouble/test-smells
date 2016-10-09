b388a2c929cbde0df2012fe4cf42f4edf718293b
Justin Searls <searls@gmail.com>
2016-10-09 15:52:40 +0800

    use Thread.list instead

--- support/ruby/async_minitest_plugin.rb
+++ support/ruby/async_minitest_plugin.rb
@@ -1,12 +1,12 @@
 module AsyncMinitestPlugin
   def before_setup
     super
-    @preexisting_threads = ObjectSpace.each_object(Thread).to_a
+    @preexisting_threads = Thread.list.dup
   end
 
   def after_teardown
     require "thwait"
-    all_threads = ObjectSpace.each_object(Thread).to_a - @preexisting_threads
+    all_threads = Thread.list - @preexisting_threads
     ThreadsWait.all_waits(all_threads)
     super
   end
